3cb8f8e06d07a51cd60ac5d00cc20d71358a106d
Chris Dinger <ding0057@umn.edu>
2025-07-12 13:15:29 +0700

    Update oracle libraries to 23.8.0.25.04

--- support/install_instantclient.sh
+++ support/install_instantclient.sh
@@ -1,9 +1,9 @@
 if [ "$TARGETPLATFORM" == "linux/amd64" ]; then
-  curl -sL -o instantclient-basic.zip https://download.oracle.com/otn_software/linux/instantclient/1912000/instantclient-basic-linux.x64-19.12.0.0.0dbru.zip
-  curl -sL -o instantclient-sdk.zip https://download.oracle.com/otn_software/linux/instantclient/1912000/instantclient-sdk-linux.x64-19.12.0.0.0dbru.zip
+  curl -sL -o instantclient-basic.zip https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-basic-linux.x64-23.8.0.25.04.zip
+  curl -sL -o instantclient-sdk.zip https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-sdk-linux.x64-23.8.0.25.04.zip
 elif [ "$TARGETPLATFORM" == "linux/arm64" ]; then
-  curl -sL -o instantclient-basic.zip https://download.oracle.com/otn_software/linux/instantclient/191000/instantclient-basic-linux.arm64-19.10.0.0.0dbru.zip
-  curl -sL -o instantclient-sdk.zip https://download.oracle.com/otn_software/linux/instantclient/191000/instantclient-sdk-linux.arm64-19.10.0.0.0dbru.zip
+  curl -sL -o instantclient-basic.zip https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-basic-linux.arm64-23.8.0.25.04.zip
+  curl -sL -o instantclient-sdk.zip https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-sdk-linux.arm64-23.8.0.25.04.zip
 fi
 
 unzip instantclient-basic.zip -d /opt
