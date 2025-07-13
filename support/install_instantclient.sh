00a3076716ee23a12a32a86ea415b2f6ad2f8b9b
Chris Dinger <ding0057@umn.edu>
2025-07-13 17:03:56 +0700

    Include sqlplus
    Some use cases expect sqlplus to be persent in order to exceute SQL
    during setup.

--- support/install_instantclient.sh
+++ support/install_instantclient.sh
@@ -1,14 +1,18 @@
 if [ "$TARGETPLATFORM" == "linux/amd64" ]; then
   curl -sL -o instantclient-basic.zip https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-basic-linux.x64-23.8.0.25.04.zip
   curl -sL -o instantclient-sdk.zip https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-sdk-linux.x64-23.8.0.25.04.zip
+  curl -sL -o instantclient-sqlplus https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-sqlplus-linux.x64-23.8.0.25.04.zip
 elif [ "$TARGETPLATFORM" == "linux/arm64" ]; then
   curl -sL -o instantclient-basic.zip https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-basic-linux.arm64-23.8.0.25.04.zip
   curl -sL -o instantclient-sdk.zip https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-sdk-linux.arm64-23.8.0.25.04.zip
+  curl -sL -o instantclient-sqlplus https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-sqlplus-linux.arm64-23.8.0.25.04.zip
 fi
 
 unzip instantclient-basic.zip -d /opt
 unzip instantclient-sdk.zip -d /opt
+unzip instantclient-sqlplus.zip -d /opt
 rm instantclient-basic.zip
 rm instantclient-sdk.zip
+rm instantclient-sqlplus.zip
 
 ln -s /opt/instantclient_* /opt/instantclient
